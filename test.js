fe62035dd19a5dbaef82c02576edd6496ddb1296
iSwanGit <emakr518@gmail.com>
2017-06-03 22:14:28 +0900

    link parse and replace http to https, request fail(cross-domain problem) ì½ì§ì¤

--- test.js
+++ test.js
@@ -1,24 +1,51 @@
+var curUrl;
+var reqUrl;
 
-function Hello() {
-    alert("SSL Strip check");
+function getUrl() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+        curUrl= tabs[0].url;
+        //alert(curUrl);
+        if (!curUrl.search('http://')) {    // http: true, https: false
+            reqUrl= curUrl.replace("http://", "https://");
+            //alert(reqUrl);           
+            //test();
+        }
+    });
 }
-/*
-document.addEventListener("DOMContentLoaded", function() { 
-     alert("Page Loaded");
-}, true);
-*/
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    //alert(tabId);
     if (changeInfo.status == 'complete') {
-        Hello();
+        getUrl();
+        //alert("asdf",chrome.webRequest.HttpHeaders);
+        alert("?");
+        
+        // 삽질중 이거 안돌아감
+        $.ajax({
+            url: reqUrl,
+            type: 'GET',
+            success: function(res) {
+                var contents= $(res.responseText);
+                var title= contents.find('title').text();
+                alert("오씨발 됐다");
+            },
+            error: function() {
+                alert("SSㅣ발");
+            }
+        });
     }
 });
 
-//chrome.webNavigation.onCompleted.addListener()
+function test() {
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function() {
+        if (this.readyState === this.DONE) {
+             // do something; the request has completed
+        }
+    }
+    xhr.open("GET", reqUrl, true );
+    //alert(xhr.readyState);
+    //alert(xhr.status);
+    //xhr.send();
+    
+}
 
-//window.addEventListener("load", Hello);
-//window.onload= Hello;
-//document.onload= Hello;
-//$(document).ready(function(){
-//    Hello();
-//});
