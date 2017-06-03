497a95c2464f987bcc2710fe4e66f078a042f0ae
iSwanGit <emakr518@gmail.com>
2017-06-03 23:06:34 +0900

    Cross-domain HTTPS request success. code trim

--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@
 2017-1H 정보보호 Project  
 HTTPS 연결이 가능한 페이지의 비보안 연결이나 SSL Strip에 의한 변조 공격을 탐지하는 것이 주 목적  
 Chrome Extension을 사용하여 구현
+
+## 시나리오
+(current commit)  
+HTTP인 경우 HTTPS로 강제하여 request 전송.  
+Response가 돌아오면 서비스 제공자의 보안문제, 혹은 SSL Strip 등의 MITM attack을 당하고 있는 중  
+올바른 response가 돌아오지 않으면 HTTPS 연결을 지원하지 않는 서버..
--- test.js
+++ test.js
@@ -4,48 +4,31 @@ var reqUrl;
 function getUrl() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
         curUrl= tabs[0].url;
-        alert(curUrl);
+        
         if (!curUrl.search('http://')) {    // http: true, https: false
             reqUrl= curUrl.replace("http://", "https://");
-            alert(reqUrl);           
-            //test();
+            
+            // jquery: cross-domain http request
+            $.ajax({
+                url: reqUrl,
+                type: 'GET',
+                success: function(res) {
+                var contents= $(res.responseText);
+                var title= contents.find('title').text();
+                    alert("오예 https 연결을 받는다구여");
+                    alert(res);     // for debug, 추후 지울것 ㅇㅇ
+                },
+                error: function() {
+                    alert("https 안 받는 구질구질한 사이트..");
+                }
+            });
         }
+        // 처음부터 https 연결인 경우에는 체크하지 않음
     });
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
         getUrl();
-        //alert("asdf",chrome.webRequest.HttpHeaders);
-        alert("?");
-        
-        // 삽질중
-        $.ajax({
-            url: reqUrl,
-            type: 'GET',
-            success: function(res) {
-                var contents= $(res.responseText);
-                var title= contents.find('title').text();
-                alert(title);   // 여전히 cross-domain 문제
-            },
-            error: function() {
-                alert("SSㅣ발");
-            }
-        });
     }
 });
-
-function test() {
-    var xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = function() {
-        if (this.readyState === this.DONE) {
-             // do something; the request has completed
-        }
-    }
-    xhr.open("GET", reqUrl, true );
-    //alert(xhr.readyState);
-    //alert(xhr.status);
-    //xhr.send();
-    
-}
-
