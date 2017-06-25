ddcb502c7e175e705b66932e69d33cb5a5591f2e
iSwanGit <emakr518@gmail.com>
2017-06-26 01:30:03 +0900

    insert net:: error handling code, readme.md update

--- readme.md
+++ readme.md
@@ -11,8 +11,13 @@ Chrome Extension을 사용하여 구현
 HTTP인 경우 HTTPS로 강제하여 request 전송.  
 1. Response가 돌아온다 (200 OK)
 - SSL 사용하나 서버의 페이지 보안 수준이 낮다
-- 혹은 SSL Strip 등의 MITM attack을 당하고 있는 중 (최초 페이지에서 미끼 링크만 strip되어있는 상태)
-2. 보안 에러가 발생한다 (net::ERR_INSECURE_CONNECTION)
-- (특히 중도에 검증할 경우) HSTS bypassing / SSL Strip 공격을 받음
+- **혹은 SSL Strip 등의 MITM attack을 당하고 있는 중 (최초 페이지에서 미끼 링크만 strip되어있는 상태)**
+2. 보안 에러가 발생한다 (net::ERR_INSECURE_RESPONSE)
+- **(특히 중도에 검증할 경우) HSTS bypassing / SSL Strip 공격을 받음**
+- 단, Self-signed Certificate 의 경우에도 이 에러가 발생.
+- (브라우저에서는 ERR_CERT_ 로 구분 가능하나, XHR에서는 INSECURE로 퉁쳐짐)
 3. 응답이 오지 않는다 (net::ERR_CONNECTION_REFUSED)
 - 서버에서 HTTPS 연결을 지원하지 않는다
+
+## 테스트를 위한 공격 툴
+[Bettercap](https://www.bettercap.org/docs/install.html)
--- test.js
+++ test.js
@@ -1,13 +1,31 @@
 var curUrl;
 var reqUrl;
 
+var errFlag= false;
+
 function getUrl() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
-        curUrl= tabs[0].url;
+        tab= tabs[0];
+        curUrl= tab.url;
         
+        chrome.webRequest.onErrorOccurred.addListener(
+                function(details) {
+                    if (errFlag == false) {
+                        // net:: error detect (details.error (string))
+                        // TODO: 에러별 구분
+                        alert("error: " + details.error);
+                        errFlag= true;
+                    }
+
+                }, {
+                    urls: ['*://*/*'],
+                    types: ['xmlhttprequest']
+                }
+            );
+
         if (!curUrl.search('http://')) {    // http: true, https: false
             reqUrl= curUrl.replace("http://", "https://");
-            
+                
             // jquery: cross-domain http request
             $.ajax({
                 url: reqUrl,
@@ -16,23 +34,23 @@ function getUrl() {
                 var contents= $(res.responseText);
                 var title= contents.find('title').text();
                     alert("오예 https 연결을 받는다구여");
-                    alert(res);
+                    //alert(res);
+                    alert(resObj.getAllResponseHeaders());
+                    alert(resObj.responseText);
                     alert(statusText, resObj);
-
-                },
-                error: function(res, statusText, errObj) {
-                    // can't detect net:: error type
-                    alert(res, statusText, errObj);
-                    alert("https 안 받는 구질구질한 사이트..");
                 }
             });
+            
         }
+        
         // 처음부터 https 연결인 경우에는 체크하지 않음
     });
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
+        errFlag= false;
         getUrl();
     }
 });
+
